0f8b0a211f542376bc117ee27610347616821f9a
Osama <osamaeletch@gmail.com>
2019-12-04 19:02:19 +0200

    everything is done

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 'use strict';
 
-const axios = require('axios').default;
+const axios = require('axios');
 const dotenv = require('dotenv');
 const logger = require('./lib/logger');
+
+dotenv.config();
 
 const twitter = require('./clients/twitter');
 const telegram = require('./clients/telegram');
 
-dotenv.config();
+function main() {
+  const randAyah = Math.floor(Math.random() * 6236) + 1;
+  logger('msg', `ayah's number: ${randAyah}`);
 
-const randAyah = Math.floor(Math.random() * 6236) + 1;
+  axios
+    .get(`http://api.alquran.cloud/v1/ayah/${randAyah}/editions/quran-uthmani`)
+    .then(res => {
+      const data = res.data.data[0];
 
-axios
-  .get(`http://api.alquran.cloud/v1/ayah/${randAyah}/editions/quran-uthmani`)
-  .then(res => {
-    const data = res.data.data[0];
-    twitter(data.text);
-    telegram(data.text);
-  })
-  .catch(err => logger('error', err));
+      twitter(data.text);
+      telegram(data.text);
+    })
+    .catch(err => logger('error', err));
+}
+
+main();
+setInterval(main, 60 * 60 * 1000);
