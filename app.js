57f86e21ef78faaa9b2a8f705cab17ce83ef7906
Osama <osamaeletch@gmail.com>
2019-12-06 09:28:00 +0200

    use phin for requests, better errors handling

--- app.js
+++ app.js
@@ -1,27 +1,47 @@
 'use strict';
 
-const axios = require('axios');
+const phin = require('phin');
 const dotenv = require('dotenv');
 const logger = require('./lib/logger');
 
 dotenv.config();
 
+if (
+  !process.env.twitterConsumerKey ||
+  !process.env.twitterConsumerSecret ||
+  !process.env.twitterAccessToken ||
+  !process.env.twitterAccessTokenSecret
+) {
+  logger('error', 'twitter keys error');
+  process.exit(1);
+}
+
+if (!process.env.telegramToken || !process.env.telegramChat) {
+  logger('error', 'telegram bot token/chat not found');
+  process.exit(1);
+}
+
 const twitter = require('./clients/twitter');
 const telegram = require('./clients/telegram');
 
-function main() {
+async function main() {
   const randAyah = Math.floor(Math.random() * 6236) + 1;
   logger('msg', `ayah's number: ${randAyah}`);
 
-  axios
-    .get(`http://api.alquran.cloud/v1/ayah/${randAyah}/editions/quran-uthmani`)
-    .then(res => {
-      const data = res.data.data[0];
+  try {
+    const res = await phin({
+      url: `http://api.alquran.cloud/v1/ayah/${randAyah}/editions/quran-uthmani`,
+      parse: 'json'
+    });
 
-      twitter(data.text);
-      telegram(data.text);
-    })
-    .catch(err => logger('error', err));
+    const data = res.body.data[0];
+    if (!data || !data.text) throw new Error(`empty data response, ayah number ${randAyah}`);
+
+    twitter(data.text);
+    telegram(data.text);
+  } catch (err) {
+    logger('error', err);
+  }
 }
 
 main();
